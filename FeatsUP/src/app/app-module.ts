3ce0c0ca95187b7921b79fac7b7630e291f3da07
Ness3n <243730@ids.upchiapas.edu.mx>
2025-11-29 16:36:55 -0600

    creacion de rama eliminar

--- FeatsUP/src/app/app-module.ts
+++ FeatsUP/src/app/app-module.ts
@@ -1,10 +1,9 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { Delete } from './delete/delete';
-import { Bienvenido } from './bienvenido/bienvenido';
 import { IniciaSesion } from './pages/inicia-sesion/inicia-sesion';
 import { CrearCuenta } from './pages/crear-cuenta/crear-cuenta';
 import { MisReservaciones } from './pages/mis-reservaciones/mis-reservaciones';
@@ -15,8 +14,6 @@ import { Gestionreservas } from './pages/gestionreservas/gestionreservas';
 @NgModule({
   declarations: [
     App,
-    Delete,
-    Bienvenido,
     IniciaSesion,
     CrearCuenta,
     MisReservaciones,
