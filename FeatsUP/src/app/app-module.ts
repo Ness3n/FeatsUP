1ef9fc81ab41f1e96ad8939042461475afcde61d
Ness3n <243730@ids.upchiapas.edu.mx>
2025-11-29 18:05:16 -0600

    testeo de bienvenida

--- FeatsUP/src/app/app-module.ts
+++ FeatsUP/src/app/app-module.ts
@@ -4,22 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import {Bienvenido} from './pages/bienvenido/bienvenido';
 import { IniciaSesion } from './pages/inicia-sesion/inicia-sesion';
 import { CrearCuenta } from './pages/crear-cuenta/crear-cuenta';
 import { MisReservaciones } from './pages/mis-reservaciones/mis-reservaciones';
 import { NuevaReservacion } from './pages/nueva-reservacion/nueva-reservacion';
 import { ConfirmacionReservacion } from './pages/confirmacion-reservacion/confirmacion-reservacion';
-import { Gestionreservas } from './pages/gestionreservas/gestionreservas';
 
 @NgModule({
   declarations: [
     App,
+    Bienvenido,
     IniciaSesion,
     CrearCuenta,
     MisReservaciones,
     NuevaReservacion,
     ConfirmacionReservacion,
-    Gestionreservas
   ],
   imports: [
     BrowserModule,
--- FeatsUP/src/app/app-routing-module.ts
+++ FeatsUP/src/app/app-routing-module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Bienvenido } from './pages/bienvenido/bienvenido';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path:'', component: Bienvenido}
+];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+/*
+// src/app/pruebas/pruebas-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Gestionreservas } from '../pruebas/gestionreservas/gestionreservas';
+
+const routes: Routes = [
+  { path: '', component: Gestionreservas }   // <- usar la clase correcta
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PruebasRoutingModule { }
+
+*/
