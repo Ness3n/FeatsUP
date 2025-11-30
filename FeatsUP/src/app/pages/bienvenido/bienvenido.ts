10ebadcbeb1aef25049259932e8e9153a61c7355
Ness3n <243730@ids.upchiapas.edu.mx>
2025-11-29 18:40:57 -0600

    Gestionavance

--- FeatsUP/src/app/pages/bienvenido/bienvenido.ts
+++ FeatsUP/src/app/pages/bienvenido/bienvenido.ts
@@ -8,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class Bienvenido {
 
+  showModal = false;              // controla el modal
+  successMessage: string | null = null;   // controla la notificación
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  showSuccess(message: string) {
+    this.successMessage = message;
+
+    setTimeout(() => {
+      this.successMessage = null;   // se oculta en 3s
+    }, 3000);
+  }
+
+  confirmDelete() {
+    // Aquí puedes eliminar realmente en tu lógica o servicio
+    this.showSuccess('Reservación de aa ss eliminada exitosamente');
+
+    this.closeModal();
+  }
 }
