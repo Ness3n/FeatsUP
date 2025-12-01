e850fdafd437f9b6b3eff9420f4d6092ad48dd49
Ness3n <243730@ids.upchiapas.edu.mx>
2025-11-30 20:14:16 -0600

    proceso bacj

---
+++ FeatsUP/Backend/backend/src/main/kotlin/com/featup/plugins/CORS.kt
@@ -0,0 +1,16 @@
+package com.featup.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
+
+fun Application.configureCORS() {
+    install(CORS) {
+        anyHost()
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+        allowHeader(HttpHeaders.ContentType)
+    }
+}
---
+++ FeatsUP/Backend/build.gradle.kts
@@ -0,0 +1,29 @@
+﻿plugins {
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.0"
+    application
+}
+
+group = "com.featup"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:2.3.3")
+    implementation("io.ktor:ktor-server-netty:2.3.3")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.3")
+    implementation("io.ktor:ktor-server-cors:2.3.3")
+    implementation("io.ktor:ktor-server-status-pages:2.3.3")
+    implementation("io.ktor:ktor-server-call-logging:2.3.3")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+    testImplementation("io.ktor:ktor-server-tests:2.3.3")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.0")
+}
+
+application {
+    mainClass.set("com.featup.ApplicationKt")
+}
