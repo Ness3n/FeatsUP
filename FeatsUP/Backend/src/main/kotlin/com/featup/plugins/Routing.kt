2e320078bd856815d72ca30cc979f5c07c0a78de
Ness3n <243730@ids.upchiapas.edu.mx>
2025-11-30 22:21:56 -0600

    Help

---
+++ FeatsUP/Backend/src/main/kotlin/com/featup/plugins/Routing.kt
@@ -0,0 +1,17 @@
+﻿package com.featup.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respond(mapOf("message" to "FeatsUP API está funcionando!"))
+        }
+        
+        get("/health") {
+            call.respond(mapOf("status" to "OK", "service" to "FeatsUP Backend"))
+        }
+    }
+}
