4970eeb23470b6ab369d99265baf5c9cc44465e8
Ness3n <243730@ids.upchiapas.edu.mx>
2025-12-08 11:13:56 -0600

    intentosemifuncional

--- FeatsUP/backend-featsUP/src/main/kotlin/com/featup/plugins/Routing.kt
+++ FeatsUP/backend-featsUP/src/main/kotlin/com/featup/plugins/Routing.kt
@@ -5,17 +5,28 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import com.featup.routes.*
 import io.ktor.server.auth.authenticate
+// IMPORTAR AQUÍ LA CLASE DE TU MODELO
+import com.featup.models.LoginRequest // <-- ¡Asegúrate de esta línea!
+import io.ktor.server.request.receive
 import io.ktor.server.routing.delete
 
 fun Application.configureRouting() {
   routing {
-    get("/") {
-      call.respondText("FeatsUP API (funciona casi todo T.T)")
+    get("/Login") {
+      call.respond("FeatsUP API (funciona casi todo T.T)")
+    }
+
+    post("/Login") { // Asumo que esta es la ruta de login
+      val datos = call.receive<LoginRequest>()
+      val email = datos.correo
+      val password = datos.contrasena
+
+      call.respond("Login exitoso para el usuario: $email")
     }
 
     authenticate("auth-jwt") {
       get("/protegido") {
-        call.respondText("Acceso autorizado")
+        call.respond("Acceso autorizado")
       }
     }
 
